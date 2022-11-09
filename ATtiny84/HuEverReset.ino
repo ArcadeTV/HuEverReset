96de790eceb4a85a538316a7b150d4d48dfa6226
ArcadeTV <info@arcade-tv.de>
2022-11-09 02:54:12 +0100

    Add files via upload

--- ATtiny84/HuEverReset.ino
+++ ATtiny84/HuEverReset.ino
@@ -1,20 +1,22 @@
 /* 
 ATtiny84 ----------------
-                _____
-       VCC  01-|°    |-14 GND
-            02-|     |-13 ENABLE (MiniDin 7)
-            03-|     |-12 D3: Run, Left (MiniDin 5)
-            04-|     |-11 D2: Select, Down (MiniDin 4)
-            05-|     |-10 D1: II, Right (MiniDin 3)
-            06-|     |-09 SELECT (MiniDin 6)
-     /RESET 07-|_____|-08 D0: I, Up (MiniDin 2)
+                     _____
+            VCC  01-|°    |-14 GND
+Jmp: Use Speaker 02-|     |-13 ENABLE (MiniDin 7)
+     Speaker (+) 03-|     |-12 D3: Run, Left (MiniDin 5)
+            N.C. 04-|     |-11 D2: Select, Down (MiniDin 4)
+            N.C. 05-|     |-10 D1: II, Right (MiniDin 3)
+            N.C. 06-|     |-09 SELECT (MiniDin 6)
+          /RESET 07-|_____|-08 D0: I, Up (MiniDin 2)
 
-low:   0xE2
-high:  0xD7
-ext.:  0xFF
-lock:  0xFF
+| Fuses: 
+| -----------
+| low:   0xE2
+| high:  0xD7
+| ext.:  0xFF
+| lock:  0xFF
 
--------------------------
+----------------------------------------------------------------------------------------------------------
 
 Description:
 Source Code for the ATtiny84 avr of the project "HuEverReset".
@@ -24,18 +26,27 @@ When the button-combo I+II+RUN+SELECT is held for 2 seconds,
 an active-low reset signal is sent from pin 7.
 
 */
+
+// S-E-T-U-P: --------------------------------------------------------------------------------------------
+// Change these settings to your needs
+
 bool testOnArduino = true;  // For development purposes set this to true.
                             // Useful for testing outside a real PC Engine console.
                             // Pins 9 (SELECT) and 13 (ENABLE) will become OUTPUTS and will always be LOW!
+bool enableSerial = false;
+// -------------------------------------------------------------------------------------------------------
 
-const byte CTRL2 = 8;       // D0: I, Up
-const byte CTRL3 = 10;      // D1: II, Right
-const byte CTRL4 = 11;      // D2: Select, Down
-const byte CTRL5 = 12;      // D3: Run, Left
-const byte CTRL6 = 9;       // SELECT
-const byte CTRL7 = 13;      // ENABLE
+const byte CTRL2 = 8;   // D0: I, Up
+const byte CTRL3 = 10;  // D1: II, Right
+const byte CTRL4 = 11;  // D2: Select, Down
+const byte CTRL5 = 12;  // D3: Run, Left
+const byte CTRL6 = 9;   // SELECT
+const byte CTRL7 = 13;  // ENABLE
 
 const byte RESET = 7;  // RESET OUT (active low)
+
+const byte useBuzzer = 2;
+const int buzzer = 3;
 
 unsigned long keyPrevMillis = 0;
 const unsigned long keySampleIntervalMs = 25;
@@ -46,11 +57,17 @@ byte prevKeyState = HIGH;  // combo is active low
 
 bool resetInProgress = false;
 bool comboActive = false;
+int buzzing = HIGH;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.flush();
-  Serial.println("Initialized!\n");
+  if (enableSerial) {
+    Serial.begin(115200);
+    Serial.flush();
+    Serial.println("Initialized!\n");
+  }
+
+  pinMode(useBuzzer, INPUT_PULLUP);
+  pinMode(buzzer, OUTPUT);
 
   pinMode(RESET, INPUT);  // init RESET pin as input
   pinMode(CTRL2, INPUT);
@@ -96,20 +113,26 @@ byte checkButtonCombo() {
 
 // called when button is kept pressed for less than 2 seconds
 void shortKeyPress() {
-  Serial.println(" -> Combo too short!");
+  if (enableSerial) {
+    Serial.println(" -> Combo too short!");
+  }
 }
 
 
 // called when button is kept pressed for more than 2 seconds
 void longKeyPress() {
-  Serial.println(" -> RESET has been triggered!");
+  if (enableSerial) {
+    Serial.println(" -> RESET has been triggered!");
+  }
 }
 
 
 // called when key goes from not pressed to pressed
 void keyPress() {
-  Serial.flush();
-  Serial.print("Combo detected!");
+  if (enableSerial) {
+    Serial.flush();
+    Serial.print("Combo detected!");
+  }
   longKeyPressCount = 0;
   comboActive = true;
 }
@@ -117,8 +140,10 @@ void keyPress() {
 
 // called when key goes from pressed to not pressed
 void keyRelease() {
-  Serial.flush();
-  Serial.print("\nCombo released.");
+  if (enableSerial) {
+    Serial.flush();
+    Serial.print("\nCombo released.");
+  }
 
   if (longKeyPressCount >= longKeyPressCountMax) {
     longKeyPress();
@@ -129,14 +154,32 @@ void keyRelease() {
 }
 
 void performReset() {
-  Serial.println("\nRESETTING!");
+  if (enableSerial) {
+    Serial.println("\nRESETTING!");
+  }
+
+  buzzing = digitalRead(useBuzzer);
+
+  if (buzzing == LOW) {
+    // alert upcoming reset:
+    tone(buzzer, 2220);
+    delay(100);
+    tone(buzzer, 1400);
+    delay(100);
+    tone(buzzer, 1675);
+    delay(100);
+    tone(buzzer, 2220);
+    delay(100);
+    noTone(buzzer);
+  }
+
+  // send /RESET signal:
   pinMode(RESET, OUTPUT);
   delay(2000);
   pinMode(RESET, INPUT);
 }
 
 void loop() {
-  // key management section
   if (millis() - keyPrevMillis >= keySampleIntervalMs) {
     keyPrevMillis = millis();
     byte currKeyState = checkButtonCombo();
